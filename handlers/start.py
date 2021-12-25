248feb97f49c55d2bc335f026574f30ea319442f
AbhimanyuRana <91766052+AbhumanyuXMusic@users.noreply.github.com>
2021-12-25 18:40:41 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,25 +9,25 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups.For Support Join our group @W2HSupport.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
+        f"""**Hᴇʏ I'ᴀᴍ {bn}, "Cᴀɴᴅʏ" Tʜᴀᴛ Lᴇᴛs Yᴏᴜ Pʟᴀʏ Mᴜsɪᴄ Iɴ Yᴏᴜʀ Tᴇʟᴇɢʀᴀᴍ Gʀᴏᴜᴘs.Dᴇᴠᴇʟᴏᴘᴇᴅ Bʏ @Candy_626.\n\n Tʜᴇ Assɪsᴛᴀɴᴛ Mᴜsᴛ Bᴇ Iɴ Yᴏᴜʀ Gʀᴏᴜᴘ Tᴏ Pʟᴀʏ Mᴜsɪᴄ Iɴ Tʜᴇ Vᴏɪᴄᴇ Cʜᴀᴛ Oғ Yᴏᴜʀ Gʀᴏᴜᴘ.\n\n /Hᴇʟᴘ Tᴏ Kɴᴏᴡ Mʏ Cᴏᴍᴍᴀɴᴅs**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Support⚡️", url="https://t.me/W2HSupport"
+                        "Sᴜᴘᴘᴏʀᴛ⚡️", url="https://t.me/AlishaSupport"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "Creater⚡️", url="https://t.me/David99q"
+                        "Cʀᴇᴀᴛᴏʀ⚡️", url="https://t.me/Candy_626"
                     ),
                     InlineKeyboardButton(
-                        "Ravan102030⚡️", url="https://t.me/Ravan102030"
+                        "Uᴘᴅᴀᴛᴇs⚡️", url="https://t.me/Pubglovers_shayri_lovers"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "Add To Your Group⚡️", url="https://t.me/W2H_Music_1_Bot?startgroup=true"
+                        "Gʀᴏᴜᴘ Mᴇ ʟᴇ Jᴀᴏ⚡️", url="https://t.me/CandyMusic_Robot?startgroup=true"
                     )
                 ]
             ]
